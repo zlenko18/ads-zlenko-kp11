83a78b3f99ed080ecf797fc3f16487f5ef789007
Ihor Zlenko <zlenko18@gmail.com>
2021-10-20 23:05:38 +0300

    Initial commit

---
+++ 1/task_1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Math;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = "); double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y = "); double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("z = "); double z = Convert.ToDouble(Console.ReadLine());
+
+            double a = 0, b = 0;
+            if (((Pow(x, 2) * y) - (Sign(Pow(x, 2) - (y * Pow(z, 3))) * Pow(Abs(Pow(x, 2) - (y * Pow(z, 3))), 1.0 / 3.0))) == 0)
+                Console.WriteLine("Ошибка");
+            else
+                a = (x + y - z) / ((Pow(x, 2) * y) - (Sign(Pow(x, 2) - (y * Pow(z, 3))) * Pow(Abs(Pow(x, 2) - (y * Pow(z, 3))), 1.0 / 3.0)));
+            if(a == 0)
+                Console.WriteLine("Ошибка");
+            else
+            {
+                b = Cos((x * y + Pow(y, 2)) / Pow(a, 2));
+                Console.WriteLine("a = " + a + "; b = " + b);
+            }
+        }
+    }
+}
---
+++ 1/task_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = "); int n = Convert.ToInt32(Console.ReadLine());
+            var listOfNumbers = new List<int>();
+            
+            for(int i = 0; i < n; i++)
+            {
+                int number = i, reversed = 0;
+                int currentNumber = number;
+                while (number > 0)
+                {
+                    reversed *= 10;
+                    reversed += number % 10;
+                    number /= 10;
+                }
+                if (currentNumber == reversed)
+                    listOfNumbers.Add(currentNumber);
+            }
+            foreach(var item in listOfNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
